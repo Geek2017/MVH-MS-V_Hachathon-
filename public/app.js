213a8c7a72e148ba27837fdecec4026801fd02c4
Geek2017 <jmlgrefaldo@bicol-u.edu.ph>
2020-06-18 17:17:49 +0800

    MVH

--- public/app.js
+++ public/app.js
@@ -4,14 +4,13 @@ angular
     .config(function($routeProvider) {
         $routeProvider
             .when('/', {
-                templateUrl: 'views/main.html'
+                templateUrl: 'views/main.html',
+                controller: "mainCtrl"
             })
             .when('/apple', {
-                templateUrl: 'views/apple.html',
-                controller: 'appleCtrl'
+                templateUrl: 'views/apple.html'
             })
             .when('/lettuce', {
-                templateUrl: 'views/lettuce.html',
-                controller: 'lettuceCtrl'
+                templateUrl: 'views/lettuce.html'
             });
     });
